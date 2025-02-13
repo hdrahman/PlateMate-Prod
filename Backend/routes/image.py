59816df59991788ea8d6d4fd837501a6421bc41f
hdrahman <haamed1.450@gmail.com>
2025-02-12 21:01:28 -0500

    added presigned url for images.

--- Backend/routes/image.py
+++ Backend/routes/image.py
@@ -12,19 +12,29 @@ router = APIRouter()
 s3_client = boto3.client(
     's3',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_access_key'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
     region_name=os.getenv('AWS_REGION')
 )
 
 bucket_name = os.getenv('AWS_BUCKET_NAME')
 
 @router.post("/upload-image")
-def upload_image(file: UploadFile = File(...), user_id =1):
+def upload_image(file: UploadFile = File(..., media_type="image/*"), user_id: int = 1):
     file_key = f'user_{user_id}/{file.filename}'
 
     try:
         s3_client.upload_fileobj(file.file, bucket_name, file_key)
-        return {"message": "Image uploaded successfully", 'file_key': file_key}
+
+        file_url = s3_client.generate_presigned_url(
+            'get_object',
+            params={'bucket': bucket_name, 'key': file_key},
+            ExpiresIn=10800 #3 hours
+        )
+        return {
+            "message": "Image uploaded successfully", 
+            'file_key': file_key,
+            'file_url': file_url
+            }
     except Exception as e:
         raise HTTPException(status_code=500, detail=f'Upload failed: {e}')
     
