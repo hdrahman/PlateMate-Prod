e54809ff0b5294f422a5176ea7a374e8ee72d9c9
hdrahman <haamed1.450@gmail.com>
2025-02-12 21:08:54 -0500

    added image routing to main.

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from routes.image import router as image_router
 
 app = FastAPI()
+app.include_router(image_router, prefix='/images', taxs={'images'})
 
 @app.get("/")
 def home():
-    return {"message": "FastAPI backend is running!"}
+    return {'message': "FastAPI connected to NEON successfully!"}
