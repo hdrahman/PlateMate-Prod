fa92e7e190202730b843794af16cbc781b34865b
hdrahman <haamed1.450@gmail.com>
2025-02-12 21:19:14 -0500

    converted food log from psycopg2 to sqlalchemy

--- Backend/models.py
+++ Backend/models.py
@@ -1,23 +1,19 @@
-import psycopg2
-from db import conn
-cursor = conn.cursor()
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy.sql import func
+from db import Base
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS food_logs(
-               id serial PRIMARY KEY,
-               user_id INT REFERENCES users(id) ON Delete CASCADE,
-               food_name TEXT NOT NULL,
-               calories INT NOT NULL,
-               proteins INT NOT NULL,
-               carbs INT NOT NULL,
-               fats INT NOT NULL,
-               image_url TEXT, --store the cloiud storage URL, not the actual image.
-               healthiness_rating INT CHECK(healthiness_rating BETWEEN 1 AND 10),
-               date TIMESTAMP DEFAULT NOW(),
-    )
-""")
+class FoodLog(Base):
+    __tablename__ = "food_logs"
 
-conn.commit()  # Save changes to the database
-print("✅ Food Logs table created successfully on Neon!")
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
+    food_name = Column(String, nullable=False)
+    calories = Column(Integer, nullable=False)
+    proteins = Column(Integer, nullable=False)
+    carbs = Column(Integer, nullable=False)
+    fats = Column(Integer, nullable=False)
+    image_url = Column(String)  # Store cloud storage URL
+    healthiness_rating = Column(Integer)
+    date = Column(DateTime, default=func.now())  # Auto timestamp
 
-
+    
